8afd381e6370620b9a50be73e4ff947d6e09a0c1
Amira Naifa J <amiranaifa3@gmail.com>
2026-06-15 12:33:24 +0700

    Membuat halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,56 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login SIJELAS</title>
+
+    @vite(['resources/css/app.css', 'resources/css/login.css'])
 </head>
 <body>
 
-    <h2>Login SIJELAS</h2>
+    <header class="navbar">
+        <img src="{{ asset('images/logo-sijelas.png') }}" alt="Belum ada potonya woi">
+    </header>
 
-    <form action="{{ route('login.process') }}" method="POST">
-        @csrf
+    <main class="login-section">
 
-        <div>
-            <label>Username</label>
-            <input type="text" name="username">
+        <div class="overlay"></div>
+
+        <div class="login-card">
+
+            <h1>LOGIN</h1>
+
+            <form action="{{ route('login.process') }}" method="POST">
+                @csrf
+
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="NAMA"
+                >
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="PASSWORD"
+                >
+
+                <select name="role">
+                    <option selected disabled>MASUK SEBAGAI</option>
+                    <option value="siswa">Siswa</option>
+                    <option value="wali_kelas">Wali Kelas</option>
+                </select>
+
+                <button type="submit">
+                    LOGIN
+                </button>
+
+            </form>
+
         </div>
 
-        <br>
-
-        <div>
-            <label>Password</label>
-            <input type="password" name="password">
-        </div>
-
-        <br>
-
-        <button type="submit">Login</button>
-
-    </form>
+    </main>
 
 </body>
 </html>
