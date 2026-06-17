3302fd35c135177d1d10579470cb82f83eee3778
yynd12 <yayayoya348@gmail.com>
2026-06-17 19:40:08 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [ 'resources/css/app.css',
+        'resources/css/login.css',
+        'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
