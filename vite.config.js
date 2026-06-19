45cd9e66dbb6931182c43c6cc524f688931fd4a6
yynd12 <yayayoya348@gmail.com>
2026-06-19 12:53:36 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: [ 'resources/css/app.css',
         'resources/css/login.css',
-        'resources/css/dashboard.css.css',
+        'resources/css/dashboard.css',
         'resources/js/app.js'],
             refresh: true,
         }),
